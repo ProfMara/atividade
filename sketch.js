386a92f1ac00b31653b709217e1716316d1a7582
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-02-24 08:31:37 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,9 @@
-var titulo, subtitulo, texto;
-var brilho;
-var grupo;
 
 function preload(){
-    //carrega as imagens
-    brilho = loadImage("a.png");
-
 
 }
 function setup() {
     createCanvas(windowWidth,windowHeight);
-    //cria grupos
-    grupo = new Group();
-    //cria a sprite
-    var sprite = createSprite(500, 500);
-    //adiciona a imagem
-    sprite.addImage(brilho);
-    //define o tamanho
-    sprite.scale = 0.1;
-    //define tempo de vida
-    sprite.lifetime = 20;
 
 }
 
@@ -31,17 +15,3 @@ function draw() {
     drawSprites();
 }
 
-function mouseClicked(){
-    //clicou com o mouse
-    var sprite = createSprite(mouseX, mouseY);
-    sprite.addImage(brilho);
-    sprite.scale = 0.1;
-    sprite.lifetime = 20;
-    grupo.add(sprite);
-}
-
-function mouseReleased(){ 
-    //soltou o mouse
-
-
-}
